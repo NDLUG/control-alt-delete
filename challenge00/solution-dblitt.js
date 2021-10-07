3c22a86e2cc407860ad8fc553bdcaf8159447792
Daniel Blittschau <d@dblitt.com>
2021-10-07 00:40:31 -0400

    Do part A lol. This was a lot of spaghetti.

--- challenge00/solution-dblitt.js
+++ challenge00/solution-dblitt.js
@@ -1,4 +1,4 @@
-// Solution to Challenge 00 Javascript (Node.js)
+// Solution to Challenge 00 - Javascript (Node.js)
 // Copyright (c) 2021 Daniel Blittschau
 
 // Run with `echo '1 1 3 6 1 1 5 2' | node solution-dblitt.js`
@@ -16,6 +16,21 @@ const programInput = [];
 rl.on('line', input => {
   programInput.push(input);
   if (programInput.length >= 1) {
+    // part A
+    partABoard = programInput[0].split(' ').map(Number);
+    // console.log(partABoard)
+    let drinkCounter = 1;
+    const drinkPossibilities = [];
+    resolvePartialBoard(partABoard, 0, drinkPossibilities);
+    let lowestAmount = drinkPossibilities[0];
+    for (let i = 1; i < drinkPossibilities.length; i++) {
+      if (drinkPossibilities[i] < lowestAmount) {
+        lowestAmount = drinkPossibilities[i];
+      }
+    }
+    console.log('Part A: ' + lowestAmount);
+
+    // part B
     const board = [];
     const boardInput = programInput[0].split(' ');
     for (let i = 0; i < boardInput.length; i++) {
@@ -25,7 +40,7 @@ rl.on('line', input => {
       });
     }
     let haveHitLastDrink = false;
-    let drinkCounter = 0;
+    let drinkCounterB = 0;
     for (let i = 0; i < board.length; i++) {
       // find the largest boi (unused) on the board
       let largestUnusedIndex = 0;
@@ -45,10 +60,10 @@ rl.on('line', input => {
         i = board.length - 2;
         haveHitLastDrink = true;
       }
-      drinkCounter++;
+      drinkCounterB++;
     }
-    // console.log('Tux must drink at least ' + drinkCounter + ' time' + ((drinkCounter == 1) ? '' : 's') + '.');
-    console.log('Part A: ' + drinkCounter);
+    // console.log('Tux must drink at least ' + drinkCounterB + ' time' + ((drinkCounterB == 1) ? '' : 's') + '.');
+    console.log('Part B: ' + drinkCounterB);
     let boardDisplay = '';
     for (let i = 0; i < board.length; i++) {
       boardDisplay += String(board[i].number);
@@ -57,9 +72,25 @@ rl.on('line', input => {
       }
     }
     // console.log('He will arrange the board to ' + boardDisplay);
-    console.log('Part B: ' + boardDisplay);
   }
 });
+
+function resolvePartialBoard(board, drinksSoFar, drinkPossibilities) {
+  // board is an array of Numbers
+  // drinksSoFar is the number of iterations so far
+  // drinkPossibilities is an array that will have all possible values
+
+  // base case??
+
+  // console.log(board)
+  if (board.length == 0)
+  drinkPossibilities.push(drinksSoFar+1)
+  // always start with the first thing
+  for (let i = 1; i <= board[0]; i++) {
+    // i starts at 1 because board values cannot be <1
+    resolvePartialBoard(board.slice(i, board.length), drinksSoFar+1, drinkPossibilities);
+  }
+}
 
 // https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
 function array_move(arr, old_index, new_index) {
