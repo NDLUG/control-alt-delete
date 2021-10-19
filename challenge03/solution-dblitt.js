36a28eec46ea11b5d5904cff6b2c6192159184cc
Daniel Blittschau <d@dblitt.com>
2021-10-19 10:58:42 -0500

    Drop dead code

--- challenge03/solution-dblitt.js
+++ challenge03/solution-dblitt.js
@@ -10,7 +10,6 @@ let data = fs.readFileSync(0, 'utf-8');
 const manifest = JSON.parse(data);
 
 let dependencyCounter = 0;
-// let dependencyCounter1 = 0;
 let dependencies = [];
 let dependencyNeedTracker = new Proxy({}, {
     get: (target, name) => name in target ? target[name] : 0
@@ -20,18 +19,9 @@ for (package in manifest) {
         dependencyNeedTracker[manifest[package][i]] ++;
     }
 }
-let terminalPackages = 0;
-
-// for (let i = 0; i < manifest['jailbreak'].length; i++) {
-//     if (dependencies.indexOf(manifest['jailbreak'][i]) !== -1) {
-//         dependencies.push(manifest['jailbreak'][i]);
-//         dependencyCounter1++;
-//     }
-// }
 
 let depthsPackagesAreBuiltAt = []
 checkDependenciesQueue = [];
-// checkDependenciesQueue.push({newDependenciesToCheck: manifest['jailbreak'], depth: 0});
 // we cant just use manifest[jailbreak] because since we counted the garbage packages that
 // werent depens, we have to get rid of them
 
@@ -42,22 +32,13 @@ for (package in manifest) {
     }
 }
 
-// function checkDependencies(newDependenciesToCheck, depth) {
 while (checkDependenciesQueue.length > 0) {
-    //console.log(depth);
     let {newDependenciesToCheck, depth, isThisNotGarbage} = checkDependenciesQueue.pop();
     if(isThisNotGarbage) depthsPackagesAreBuiltAt.push(depth)
-    if (newDependenciesToCheck.length === 0) {
-        terminalPackages++;
-    }
-    // if (depth === 0)
-    // console.log(newDependenciesToCheck.length)
     for (let i = 0; i < newDependenciesToCheck.length; i++) {
         dependencyNeedTracker[newDependenciesToCheck[i]]--;
         if (dependencies.indexOf(newDependenciesToCheck[i]) === -1) {
             dependencies.push(newDependenciesToCheck[i]);
-            // dependencyCounter++;
-            // checkDependencies(manifest[newDependenciesToCheck[i]], depth + 1);
         }
         if (dependencyNeedTracker[newDependenciesToCheck[i]] === 0) {
             checkDependenciesQueue.push({newDependenciesToCheck: manifest[newDependenciesToCheck[i]], depth: depth + 1, isThisNotGarbage: isThisNotGarbage});
@@ -77,11 +58,5 @@ for (let i = 1; i < possibleMaxConcurrencies.length; i++) {
     }
 }
 
-// console.log(counts);
 console.log('Number of Dependencies: ' + dependencies.length);
 console.log('Maximum Concurrency: ' + maxConcurrency);
-// console.log(dependencies.length)
-// console.log(dependencyNeedTracker)
-// console.log(dependencies);
-// console.log(terminalPackages)
-
